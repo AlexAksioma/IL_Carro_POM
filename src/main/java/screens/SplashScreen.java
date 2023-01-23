cae3299d5c7337a46e430ba9062b8fb1de31dfdb
AlexAksioma <alexmedqwerty@gmail.com>
2023-01-23 17:19:23 +0200

    23.01.2023

---
+++ src/main/java/screens/SplashScreen.java
@@ -0,0 +1,20 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.support.FindBy;
+
+import javax.xml.xpath.XPath;
+
+public class SplashScreen extends BaseScreen{
+
+    public SplashScreen(AppiumDriver<MobileElement> driver) {
+        super(driver);
+    }
+    @FindBy(xpath = "//*[@resource-id='com.telran.ilcarro:id/versionText']")
+    MobileElement versionTextView;
+
+    public String getCurrentVersion(){
+        return versionTextView.getText();
+    }
+}
